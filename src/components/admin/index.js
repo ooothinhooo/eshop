677a81a45881f67dbb62727bdd2f6f19b94075be
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2024-04-10 17:11:57 +0700

    update 2

--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -1,6 +1,6 @@
 export { default as AddProduct } from "./addProduct/AddProduct";
 export { default as ChangeOrderStatus } from "./changeOrderStatus/ChangeOrderStatus";
-export { default as Home } from "./Home/Home";
+// export { default as Home } from "./Home/Home";
 export { default as NavBar } from "./navbar/NavBar";
 export { default as OrderDetails } from "./orderDetails/OrderDetails";
 export { default as Orders } from "./orders/Orders";
